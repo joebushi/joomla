4cedf43c999806959335a559b05d131b62a498e4
rhuk <rhuk@6f6e1ebd-4c2b-0410-823f-f34bde69bce9>
2005-09-15 02:01:09 +0000

    test
    git-svn-id: http://joomlacode.org/svn/joomla/development/branches/1.0.x@45 6f6e1ebd-4c2b-0410-823f-f34bde69bce9

--- CHANGELOG.php
+++ CHANGELOG.php
@@ -1,6 +1,8 @@
 <?php
 /**
 * @version $Id$
+* Test $Rev$
+* Test2 $Revision$
 * @package Joomla
 * @copyright Copyright (C) 2005 Open Source Matters. All rights reserved.
 * @license http://www.gnu.org/copyleft/gpl.html GNU/GPL, see LICENSE.php
