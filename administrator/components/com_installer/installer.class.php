0f00e9999031e26191c44b3c6829fd05cdce120e
Levis <Levis@6f6e1ebd-4c2b-0410-823f-f34bde69bce9>
2005-09-20 15:11:17 +0000

    + added a chmod for Linux and Unix server - function deldir
    git-svn-id: http://joomlacode.org/svn/joomla/development/branches/1.0@202 6f6e1ebd-4c2b-0410-823f-f34bde69bce9

--- administrator/components/com_installer/installer.class.php
+++ administrator/components/com_installer/installer.class.php
@@ -483,15 +483,18 @@ function cleanupInstall( $userfile_name, $resultdir) {
 
 function deldir( $dir ) {
 	$current_dir = opendir( $dir );
+	$old_umask = umask(0);
 	while ($entryname = readdir( $current_dir )) {
 		if ($entryname != '.' and $entryname != '..') {
 			if (is_dir( $dir . $entryname )) {
 				deldir( mosPathName( $dir . $entryname ) );
 			} else {
+                @chmod($dir . $entryname, 0777);
 				unlink( $dir . $entryname );
 			}
 		}
 	}
+	umask($old_umask);
 	closedir( $current_dir );
 	return rmdir( $dir );
 }
