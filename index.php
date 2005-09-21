c7e638fe0384b99cdbf3e154c3c938f8a6f98ab5
stingrey <stingrey@6f6e1ebd-4c2b-0410-823f-f34bde69bce9>
2005-09-21 16:27:10 +0000

    Final preparation for release
    git-svn-id: http://joomlacode.org/svn/joomla/development/branches/1.0@225 6f6e1ebd-4c2b-0410-823f-f34bde69bce9

--- index.php
+++ index.php
@@ -23,14 +23,14 @@ if (!file_exists( 'configuration.php' ) || filesize( 'configuration.php' ) < 10)
 include_once( 'globals.php' );
 require_once( 'configuration.php' );
 require_once( 'includes/joomla.php' );
-/*
+
 //Installation sub folder check, removed for work with CVS
 if (file_exists( 'installation/index.php' )) {	
 	define( '_INSTALL_CHECK', 1 );
 	include ('offline.php');
 	exit();
 }
-*/
+
 // displays offline/maintanance page or bar
 if ($mosConfig_offline == 1) {
 	require( 'offline.php' );
