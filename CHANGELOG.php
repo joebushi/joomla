22ecff742c176bd8c24f0ecd3b3b5801f2e6dd0e
jick <jick@6f6e1ebd-4c2b-0410-823f-f34bde69bce9>
2005-10-01 18:13:06 +0000

    git-svn-id: http://joomlacode.org/svn/joomla/development/branches/1.0@290 6f6e1ebd-4c2b-0410-823f-f34bde69bce9

--- CHANGELOG.php
+++ CHANGELOG.php
@@ -30,6 +30,9 @@ Legend:
 - -> Removed
 ! -> Note
 
+01-Oct-2005 Arno Zijlstra
+ ^ Changed OSM banner 2 a little to show banner changing
+ 
 01-Oct-2005 Levis Bisson
  # Fixed absolute backend redirection
  # Fixed artf1311 : Banners not showing / returning PHP error
