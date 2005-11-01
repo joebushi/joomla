300343e26cd35cbf8ae20c5409d86c3737ae660f
stingrey <stingrey@6f6e1ebd-4c2b-0410-823f-f34bde69bce9>
2005-11-01 13:38:12 +0000

    # Fixed artf1888 : linkable [category|section] URL incorrect
    git-svn-id: http://joomlacode.org/svn/joomla/development/branches/1.0@788 6f6e1ebd-4c2b-0410-823f-f34bde69bce9

--- CHANGELOG.php
+++ CHANGELOG.php
@@ -32,6 +32,7 @@ Legend:
 ! -> Note
 
 01-Nov-2005 Rey Gigataras
+ # Fixed artf1888 : linkable [category|section] URL incorrect
  # Fixed artf1620 : Hardcoded words in pdf.php
  # Fixed artf1887 : Content: Bug in creation date generation
 
