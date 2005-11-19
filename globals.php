e15e86b9524e9b7a453cc78dcab6ad26321d123b
eddieajau <eddieajau@6f6e1ebd-4c2b-0410-823f-f34bde69bce9>
2005-11-19 05:47:24 +0000

    git-svn-id: http://joomlacode.org/svn/joomla/development/branches/1.0@1125 6f6e1ebd-4c2b-0410-823f-f34bde69bce9

--- globals.php
+++ globals.php
@@ -1,26 +1,97 @@
 <?php
 /**
-* @version $Id$
-* @package Joomla
-* @copyright Copyright (C) 2005 Open Source Matters. All rights reserved.
-* @license http://www.gnu.org/copyleft/gpl.html GNU/GPL, see LICENSE.php
-* Joomla! is free software and parts of it may contain or be derived from the
-* GNU General Public License or other free or open source software licenses.
-* See COPYRIGHT.php for copyright notices and details.
-*/
+ * @version $Id$
+ * @package Joomla
+ * @copyright Copyright (C) 2005 Open Source Matters. All rights reserved.
+ * @license http://www.gnu.org/copyleft/gpl.html GNU/GPL, see LICENSE.php
+ * Joomla! is free software and parts of it may contain or be derived from the
+ * GNU General Public License or other free or open source software licenses.
+ * See COPYRIGHT.php for copyright notices and details.
+ */
 
-if (!ini_get('register_globals')) {
-	while (list( $key, $value ) = each( $_FILES )) $GLOBALS[$key] = $value;
-	while (list( $key, $value ) = each( $_ENV )) $GLOBALS[$key] = $value;
-	while (list( $key, $value ) = each( $_GET )) $GLOBALS[$key] = $value;
-	while (list( $key, $value ) = each( $_POST )) $GLOBALS[$key] = $value;
-	while (list( $key, $value ) = each( $_COOKIE )) $GLOBALS[$key] = $value;
-	while (list( $key, $value ) = each( $_SERVER )) $GLOBALS[$key] = $value;	
-	
-	if (isset($_SESSION)) {
-		while (list( $key, $value ) = @each( $_SESSION )) $GLOBALS[$key] = $value;
-	}	
-	
+/**
+ * Use 0 to emulate register_globals = on
+ * 
+ * Use 1 to emulate regsiter_globals = off
+ */
+define( 'RG_EMULATION', 1 );
+
+/**
+ * Adds an array to the GLOBALS array and checks that the GLOBALS variable is
+ * not being attacked
+ * @param array
+ * @param boolean True if the array is to be added to the GLOBALS
+ */
+function checkInputArray( &$array, $globalise=false ) {
+	static $banned = array( '_files', '_env', '_get', '_post', '_cookie', '_server', '_sessions', 'globals' );
+
+	foreach ($array as $key => $value) {
+		if (in_array( strtolower( $key ), $banned )) {
+			die( 'Illegal variable <b>' . implode( '</b> or <b>', $banned ) . '</b> passed to script.' );
+		}
+		if ($globalise) {
+			$GLOBALS[$key] = $value;
+		}
+	}
+}
+
+/**
+ * Emulates register globals = off
+ */
+function unregisterGlobals () {
+	checkInputArray( $_FILES );
+	checkInputArray( $_ENV );
+	checkInputArray( $_GET );
+	checkInputArray( $_POST );
+	checkInputArray( $_COOKIE );
+	checkInputArray( $_SERVER );
+
+	if (isset( $_SESSION )) {
+		checkInputArray( $_SESSION );
+	}
+
+	$REQUEST = $_REQUEST;
+	$GET = $_GET;
+	$POST = $_POST;
+	$COOKIE = $_COOKIE;
+	if (isset ( $_SESSION )) {
+		$SESSION = $_SESSION;
+	}
+	$FILES = $_FILES;
+	$ENV = $_ENV;
+	$SERVER = $_SERVER;
+	foreach ($GLOBALS as $key => $value) {
+		if ( $key != 'GLOBALS' ) {
+			unset ( $GLOBALS [ $key ] );
+		}
+	}
+	$_REQUEST = $REQUEST;
+	$_GET = $GET;
+	$_POST = $POST;
+	$_COOKIE = $COOKIE;
+	if (isset ( $SESSION )) {
+		$_SESSION = $SESSION;
+	}
+	$_FILES = $FILES;
+	$_ENV = $ENV;
+	$_SERVER = $SERVER;
+}
+
+/**
+ * Emulates register globals = off
+ */
+function registerGlobals() {
+	checkInputArray( $_FILES, true );
+	checkInputArray( $_ENV, true );
+	checkInputArray( $_GET, true );
+	checkInputArray( $_POST, true );
+	checkInputArray( $_COOKIE, true );
+	checkInputArray( $_SERVER, true );
+
+	if (isset( $_SESSION )) {
+		checkInputArray( $_SESSION, true );
+	}
+
 	foreach ($_FILES as $key => $value){
 		$GLOBALS[$key] = $_FILES[$key]['tmp_name'];
 		foreach ($value as $ext => $value2){
@@ -28,5 +99,11 @@ if (!ini_get('register_globals')) {
 			$GLOBALS[$key2] = $value2;
 		}
 	}
+}
+
+if (RG_EMULATION != 1 && ini_get('register_globals') == 0) {
+	registerGlobals();
+} else {
+	unregisterGlobals();
 }
 ?>
