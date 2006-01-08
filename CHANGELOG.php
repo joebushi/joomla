45f049a85eb5e97a172c5ef1327422b84f8cabbb
stingrey <stingrey@6f6e1ebd-4c2b-0410-823f-f34bde69bce9>
2006-01-08 06:41:36 +0000

    # Fixed DOMIT bugs identified here: sarahk.pcpropertymanager.com/blog/using-domit-rss/225/  # Fixed artf2945 : Undefined constant in php_http_exceptions.php
    git-svn-id: http://joomlacode.org/svn/joomla/development/branches/1.0@1701 6f6e1ebd-4c2b-0410-823f-f34bde69bce9

--- CHANGELOG.php
+++ CHANGELOG.php
@@ -41,6 +41,10 @@ Low Level Threat
 
 ---
 
+08-Jan-2005 Rey Gigataras
+ # Fixed DOMIT bugs identified here: sarahk.pcpropertymanager.com/blog/using-domit-rss/225/
+ # Fixed artf2945 : Undefined constant in php_http_exceptions.php
+
 07-Jan-2005 Rey Gigataras
  # Fixed artf2933 : Pathway problem on Windows
 
--- includes/domit/php_text_cache.php
+++ includes/domit/php_text_cache.php
@@ -109,7 +109,9 @@ class php_text_cache extends php_http_connector {
 	* @param boolean True if an HTTP client is to be used to establish the connection
 	*/
 	function useHTTPClient($truthVal) {
-		$this->doUseHTTPClient = truthVal;
+		// fixes bug identified here: sarahk.pcpropertymanager.com/blog/using-domit-rss/225/
+		//$this->doUseHTTPClient = truthVal;
+		$this->doUseHTTPClient = $truthVal;
 	} //useHTTPClient
 
 	/**
