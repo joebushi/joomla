08a99f5b69633a98265ba7c45d2aa71b18aba148
stingrey <stingrey@6f6e1ebd-4c2b-0410-823f-f34bde69bce9>
2006-02-08 15:36:45 +0000

    Preparation for 1.0.8 Beta 1 Packaging
    git-svn-id: http://joomlacode.org/svn/joomla/development/branches/1.0@2262 6f6e1ebd-4c2b-0410-823f-f34bde69bce9

--- includes/version.php
+++ includes/version.php
@@ -21,17 +21,17 @@ class joomlaVersion {
 	/** @var int Main Release Level */
 	var $RELEASE 	= '1.0';
 	/** @var string Development Status */
-	var $DEV_STATUS = 'Stable';
+	var $DEV_STATUS = 'Beta 1';
 	/** @var int Sub Release Level */
-	var $DEV_LEVEL 	= '7';
+	var $DEV_LEVEL 	= '8';
 	/** @var int build Number */
 	var $BUILD	 	= '$Revision$';
 	/** @var string Codename */
-	var $CODENAME 	= 'Sunburst';
+	var $CODENAME 	= 'Sunshade';
 	/** @var string Date */
-	var $RELDATE 	= '15-Jan-2006';
+	var $RELDATE 	= '08-Feb-2006';
 	/** @var string Time */
-	var $RELTIME 	= '20:00';
+	var $RELTIME 	= '**:**';
 	/** @var string Timezone */
 	var $RELTZ 		= 'UTC';
 	/** @var string Copyright Text */
