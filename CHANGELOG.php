b6f66d67d18caf22f99d06d1b3c76ab5df1a2e1f
stingrey <stingrey@6f6e1ebd-4c2b-0410-823f-f34bde69bce9>
2006-02-12 11:07:00 +0000

    # Fixed artf3220 : Search bug: No way to update referenced search component
    git-svn-id: http://joomlacode.org/svn/joomla/development/branches/1.0@2304 6f6e1ebd-4c2b-0410-823f-f34bde69bce9

--- CHANGELOG.php
+++ CHANGELOG.php
@@ -86,6 +86,7 @@ Medium Level Threat
 
 12-Feb-2006 Rey Gigataras
  * SECURITY [ Low Level ]: Multiple Admin SQL Injection Vulnerabilities
+ # Fixed artf3220 : Search bug: No way to update referenced search component
  # Fixed artf3438 : RSS Feed Created it not base on the same encoding of the content
  # Fixed artf3108 : Joomla 1.0.7 core SEF bug gives 404 on homepage
  # Fixed artf3169 : RSS feeds does not work with SEF disabled
