4fcc9216e00c442eb123581d3b6c3cdffea9bf80
akede <akede@6f6e1ebd-4c2b-0410-823f-f34bde69bce9>
2006-07-03 17:01:01 +0000

    Integrated patTemplate use
    git-svn-id: http://joomlacode.org/svn/joomla/development/branches/patTemplate_test@4170 6f6e1ebd-4c2b-0410-823f-f34bde69bce9

--- components/com_content/view/article/tmpl/article.php
+++ components/com_content/view/article/tmpl/article.php
@@ -15,10 +15,120 @@
 // no direct access
 defined('_JEXEC') or die('Restricted access');
 
-		// Process the content plugins
-		JPluginHelper::importPlugin('content');
-		$results = $app->triggerEvent('onPrepareContent', array (& $article, & $params, $page));
 
+jimport( 'joomla.template.template');
+
+// Process the content plugins
+JPluginHelper::importPlugin('content');
+$results = $app->triggerEvent('onPrepareContent', array (& $article, & $params, $page));
+
+
+// var init
+$linkOn = '';
+$linkText = '';
+
+// Build the link and text of the readmore button
+if ($params->get('readmore') || $params->get('link_titles')) {
+	if ($params->get('intro_only')) {
+		// Check to see if the user has access to view the full article
+		if ($article->access <= $user->get('gid')) {
+			$Itemid = JContentHelper::getItemid($article->id);
+			$linkOn = sefRelToAbs("index.php?option=com_content&amp;task=view&amp;id=".$article->id."&amp;Itemid=".$Itemid);
+
+			if (@$article->readmore) {
+			// text for the readmore link
+				$linkText = JText::_('Read more...');
+			}
+		} else {
+			$linkOn = sefRelToAbs("index.php?option=com_registration&amp;task=register");
+
+			if (@$article->readmore) {
+			// text for the readmore link if accessible only if registered
+				$linkText = JText::_('Register to read more...');
+			}
+		}
+	}
+}
+
+
+// Popup pages get special treatment for page titles
+if ($params->get('popup') && $type =! 'html') {
+	$doc->setTitle($app->getCfg('sitename').' - '.$article->title);
+}
+
+$tmpl =& new JTemplate;
+$tmpl->setRoot( JPATH_COM_CONTENT .DS. 'view' .DS. 'article' .DS. 'tmpl' );
+$tmpl->parse( 'article.html' );
+	
+// I don't know if we have a function to move parameter to an object
+// but this worked 
+$p = array('item_title','pdf','print','email','link_titles','popup','icons','readmore','section','category','author','url','createdate','modifydate');
+foreach ($p as $elm) {
+	$tmpl->addVar('article','params_'.$elm,$params->get($elm));
+}	
+$ppe = $params->get('pdf') || $params->get('print') || $params->get('email');
+$tmpl->addVar('article','ppe',$ppe);
+
+// fire the plugins
+$results = $app->triggerEvent('onAfterDisplayTitle', array (& $article, & $params, $page));
+$results_onAfterDisplayTitle =  trim(implode("\n", $results));
+// Display the output from the onBeforeDisplayContent event
+$onBeforeDisplayContent = $app->triggerEvent('onBeforeDisplayContent', array (& $article, & $params, $page));
+$results_onBeforeDisplayContent =  trim(implode("\n", $onBeforeDisplayContent));
+// Fire the after display content event
+$onAfterDisplayContent = $app->triggerEvent('onAfterDisplayContent', array (& $article, & $params, $page));
+$results_onAfterDisplayContent = trim(implode("\n", $onAfterDisplayContent));
+
+	// TODO: add output to the template
+
+$tmpl->addVar('article','results_onAfterDisplayTitle',$results_onAfterDisplayTitle);
+$tmpl->addVar('article','results_onBeforeDisplayContent',$results_onBeforeDisplayContent);
+$tmpl->addVar('article','results_onAfterDisplayContent',$results_onAfterDisplayContent);
+
+
+//copy article information to template 
+$article->created = mosFormatDate($article->created);
+$article->modified = mosFormatDate($article->modified);
+$tmpl->addObject('article',$article,'A_');
+
+// links an other stuff			
+$tmpl->addVar('article','canedit',$access->canEdit);
+$tmpl->addVar('article','linkon',$linkOn);
+$tmpl->addVar('article','linktext',$linkText);
+
+// PDF Icon
+if ($params->get('icons')) {
+	$image = mosAdminMenus::ImageCheck('pdf_button.png', '/images/M_images/', NULL, NULL, JText::_('PDF'), JText::_('PDF'));
+} else {
+	$image = JText::_('PDF').'&nbsp;';
+}
+$tmpl->addVar('article','pdf_image',$image);
+$tmpl->addVar('article','nojs',$noJS);
+$pdf_status = 'status=no,toolbar=no,scrollbars=yes,titlebar=no,menubar=no,resizable=yes,width=640,height=480,directories=no,location=no';
+$pdf_link = 'index2.php?option=com_content&amp;id='.$article->id.'&amp;format=pdf';
+$tmpl->addVar('article','pdf_status',$pdf_status);
+$tmpl->addVar('article','pdf_link',$pdf_link);
+
+// Print
+$print_status = 'status=no,toolbar=no,scrollbars=yes,titlebar=no,menubar=no,resizable=yes,width=640,height=480,directories=no,location=no';
+$print_link = $app->getBaseURL().'index2.php?option=com_content&amp;task=view&amp;id='.$article->id.'&amp;Itemid='.$Itemid.'&amp;pop=1&amp;page='.@ $page;
+$tmpl->addVar('article','print_Link',$print_link);
+if ( $params->get( 'icons' ) ) {
+	$image = mosAdminMenus::ImageCheck( 'printButton.png', '/images/M_images/', NULL, NULL, JText::_( 'Print' ), JText::_( 'Print' ) );
+} else {
+	$image = JText::_( 'ICON_SEP' ) .'&nbsp;'. JText::_( 'Print' ) .'&nbsp;'. JText::_( 'ICON_SEP' );
+}
+$tmpl->addVar('article','print_image',$image);
+$tmpl->addVar('article','print_status',$print_status);
+
+// EMAIL
+	// TODO: javascript must in head
+
+
+//display the template			
+$tmpl->display('article');
+
+/**
 		// Build the link and text of the readmore button
 		if ($params->get('readmore') || $params->get('link_titles')) {
 			if ($params->get('intro_only')) {
@@ -137,5 +247,5 @@ defined('_JEXEC') or die('Restricted access');
 
 		// displays close button in pop-up window
 		mosHTML::CloseButton($params, $noJS);
-
+*/
 ?>
