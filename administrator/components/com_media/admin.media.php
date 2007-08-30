0690057c80da23eb765e46bbcd1f5278e94ed193
louis <louis@6f6e1ebd-4c2b-0410-823f-f34bde69bce9>
2007-08-30 21:04:49 +0000

    git-svn-id: http://joomlacode.org/svn/joomla/development/branches/media_manager_mvc@8653 6f6e1ebd-4c2b-0410-823f-f34bde69bce9

--- administrator/components/com_media/admin.media.php
+++ administrator/components/com_media/admin.media.php
@@ -95,6 +95,13 @@ class MediaController extends JController
 		// Display the view
 		$view->display();
 	}
+
+	function ftpValidate()
+	{
+		// Set FTP credentials, if given
+		jimport('joomla.client.helper');
+		JClientHelper::setCredentialsFromRequest('ftp');
+	}
 }
 
 $cmd = JRequest::getCmd('task', null);
