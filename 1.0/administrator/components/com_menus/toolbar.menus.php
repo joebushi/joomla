6933583bba0930ba19ffe360f534e16220cf4e1c
willebil <willebil@6f6e1ebd-4c2b-0410-823f-f34bde69bce9>
2008-01-16 21:41:41 +0000

    git-svn-id: http://joomlacode.org/svn/joomla/development/tags/1.0.14RC1@9952 6f6e1ebd-4c2b-0410-823f-f34bde69bce9

---
+++ 1.0/administrator/components/com_menus/toolbar.menus.php
@@ -0,0 +1,87 @@
+<?php
+/**
+* @version $Id$
+* @package Joomla
+* @subpackage Menus
+* @copyright Copyright (C) 2005 Open Source Matters. All rights reserved.
+* @license http://www.gnu.org/copyleft/gpl.html GNU/GPL, see LICENSE.php
+* Joomla! is free software. This version may have been modified pursuant
+* to the GNU General Public License, and as distributed it includes or
+* is derivative of works licensed under the GNU General Public License or
+* other free or open source software licenses.
+* See COPYRIGHT.php for copyright notices and details.
+*/
+
+// no direct access
+defined( '_VALID_MOS' ) or die( 'Restricted access' );
+
+require_once( $mainframe->getPath( 'toolbar_html' ) );
+require_once( $mainframe->getPath( 'toolbar_default' ) );
+
+switch ($task) {
+	case 'new':
+		TOOLBAR_menus::_NEW();
+		break;
+
+	case 'movemenu':
+		TOOLBAR_menus::_MOVEMENU();
+		break;
+
+	case 'copymenu':
+		TOOLBAR_menus::_COPYMENU();
+		break;
+
+	case 'edit':
+		$cid 	= josGetArrayInts( 'cid' );
+		$path 	= $mosConfig_absolute_path .'/administrator/components/com_menus/';
+
+		if ( $cid[0] ) {
+			$query = "SELECT type"
+			. "\n FROM #__menu"
+			. "\n WHERE id = " . (int) $cid[0]
+			;
+			$database->setQuery( $query );
+			$type = $database->loadResult();
+			$item_path  = $path . $type .'/'. $type .'.menubar.php';
+
+			if ( $type ) {
+				if ( file_exists( $item_path  ) ) {
+					require_once( $item_path  );
+				} else {
+					TOOLBAR_menus::_EDIT();
+				}
+			} else {
+				echo $database->stderr();
+			}
+		} else {
+			$type 		= strval( mosGetParam( $_REQUEST, 'type', null ) );
+			$item_path  = $path . $type .'/'. $type .'.menubar.php';
+
+			if ( $type ) {
+				if ( file_exists( $item_path ) ) {
+					require_once( $item_path  );
+				} else {
+					TOOLBAR_menus::_EDIT();
+				}
+			} else {
+				TOOLBAR_menus::_EDIT();
+			}
+		}
+		break;
+
+	default:
+		$type 		= strval( mosGetParam( $_REQUEST, 'type' ) );
+		$item_path  = $path . $type .'/'. $type .'.menubar.php';
+
+		if ( $type ) {
+			if ( file_exists( $item_path ) ) {
+				require_once( $item_path );
+			} else {
+				TOOLBAR_menus::_DEFAULT();
+			}
+		} else {
+			TOOLBAR_menus::_DEFAULT();
+		}
+		break;
+}
+?>
