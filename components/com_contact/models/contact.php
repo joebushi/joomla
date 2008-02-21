9d9d40d2478e7b4ccda730d05b2301b75daf63d9
ian <ian@6f6e1ebd-4c2b-0410-823f-f34bde69bce9>
2008-02-21 13:17:12 +0000

    # [#9579] Contact Send-Email Form Routing to Wrong URL
    git-svn-id: http://joomlacode.org/svn/joomla/development/releases/1.5@10049 6f6e1ebd-4c2b-0410-823f-f34bde69bce9

--- components/com_contact/models/contact.php
+++ components/com_contact/models/contact.php
@@ -38,7 +38,9 @@ class ContactModelContact extends JModel
 		$groupBy	= @$options['group by'];
 		$orderBy	= @$options['order by'];
 
-		$select = 'a.*, cc.title as category_name';
+		$select = 'a.*, cc.title as category_name, '
+		. ' CASE WHEN CHAR_LENGTH(a.alias) THEN CONCAT_WS(\':\', a.id, a.alias) ELSE a.id END as slug, '
+		. ' CASE WHEN CHAR_LENGTH(cc.alias) THEN CONCAT_WS(\':\', cc.id, cc.alias) ELSE cc.id END AS catslug ';
 		$from	= '#__contact_details AS a';
 
 		$joins[] = 'INNER JOIN #__categories AS cc on cc.id = a.catid';
