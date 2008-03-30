468dd7ebf441a6dfc7bfbb96d728a78c475df301
roscohead <roscohead@6f6e1ebd-4c2b-0410-823f-f34bde69bce9>
2008-03-30 22:21:48 +0000

    RefactRefactor com_contact
    git-svn-id: http://joomlacode.org/svn/joomla/development/branches/refactoring@10185 6f6e1ebd-4c2b-0410-823f-f34bde69bce9

--- administrator/components/com_contact/tables/contact.php
+++ administrator/components/com_contact/tables/contact.php
@@ -3,7 +3,7 @@
  * @version		$Id$
  * @package		Joomla
  * @subpackage	Contact
- * @copyright	Copyright (C) 2005 - 2008 Open Source Matters. All rights reserved.
+ * @copyright	Copyright (C) 2005 - 2007 Open Source Matters. All rights reserved.
  * @license		GNU/GPL, see LICENSE.php
  * Joomla! is free software. This version may have been modified pursuant to the
  * GNU General Public License, and as distributed it includes or is derivative
@@ -13,7 +13,7 @@
  */
 
 // Check to ensure this file is included in Joomla!
-defined('_JEXEC') or die( 'Restricted access' );
+defined('_JEXEC') or die();
 
 
 /**
@@ -105,13 +105,10 @@ class TableContact extends JTable
 			$this->webpage = 'http://'.$this->webpage;
 		}
 
-		if(empty($this->alias)) {
-			$this->alias = $this->name;
-		}
-		$this->alias = JFilterOutput::stringURLSafe($this->alias);
-		if(trim(str_replace('-','',$this->alias)) == '') {
-			$datenow =& JFactory::getDate();
-			$this->alias = $datenow->toFormat("%Y-%m-%d-%H-%M-%S");
+		$alias = JFilterOutput::stringURLSafe($this->name);
+
+		if(empty($this->alias) || $this->alias === $alias ) {
+			$this->alias = $alias;
 		}
 
 		return true;
