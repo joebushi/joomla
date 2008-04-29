656158f0587d51858012fd493304aa708151bcc9
roscohead <roscohead@6f6e1ebd-4c2b-0410-823f-f34bde69bce9>
2008-04-29 11:22:52 +0000

    Refactor com_content
    git-svn-id: http://joomlacode.org/svn/joomla/development/branches/refactoring@10244 6f6e1ebd-4c2b-0410-823f-f34bde69bce9

---
+++ administrator/components/com_content/views/articles/view.html.php
@@ -0,0 +1,46 @@
+<?php
+/**
+* @version		$Id: $
+* @package		Joomla
+* @subpackage	Content
+* @copyright	Copyright (C) 2005 - 2008 Open Source Matters. All rights reserved.
+* @license		GNU/GPL, see LICENSE.php
+* Joomla! is free software. This version may have been modified pursuant
+* to the GNU General Public License, and as distributed it includes or
+* is derivative of works licensed under the GNU General Public License or
+* other free or open source software licenses.
+* See COPYRIGHT.php for copyright notices and details.
+*/
+
+// Check to ensure this file is included in Joomla!
+defined('_JEXEC') or die();
+
+jimport( 'joomla.application.component.view');
+
+/**
+ * HTML View class for the Articles component
+ *
+ * @static
+ * @package		Joomla
+ * @subpackage	Content
+ * @since 1.0
+ */
+class ContentViewArticles extends JView
+{
+	function display($tpl = null)
+	{
+		$rows		= & $this->get( 'Data');
+		$total		= & $this->get( 'Total');
+		$pagination = & $this->get( 'Pagination' );
+		$filter		= & $this->get( 'Filter' );
+
+		$redirect			= $filter->sectionid;
+
+		$this->assignRef('redirect',	$redirect);
+		$this->assignRef('rows',		$rows);
+		$this->assignRef('pagination',	$pagination);
+		$this->assignRef('filter',		$filter);
+
+		parent::display($tpl);
+	}
+}
