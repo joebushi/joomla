6d45dcc2eb070ad9b5ca0a77e8d48d47c6143265
roscohead <roscohead@6f6e1ebd-4c2b-0410-823f-f34bde69bce9>
2008-05-31 02:32:11 +0000

    Refactor com_config
    git-svn-id: http://joomlacode.org/svn/joomla/development/branches/refactoring@10359 6f6e1ebd-4c2b-0410-823f-f34bde69bce9

--- administrator/components/com_config/config.php
+++ administrator/components/com_config/config.php
@@ -1,28 +1,25 @@
 <?php
 /**
- * @version		$Id$
+ * @version		$Id: $
  * @package		Joomla
- * @subpackage	Config
+ * @subpackage	Cache
  * @copyright	Copyright (C) 2005 - 2008 Open Source Matters. All rights reserved.
  * @license		GNU/GPL, see LICENSE.php
- * Joomla! is free software. This version may have been modified pursuant to the
- * GNU General Public License, and as distributed it includes or is derivative
- * of works licensed under the GNU General Public License or other free or open
- * source software licenses. See COPYRIGHT.php for copyright notices and
- * details.
+ * Joomla! is free software. This version may have been modified pursuant
+ * to the GNU General Public License, and as distributed it includes or
+ * is derivative of works licensed under the GNU General Public License or
+ * other free or open source software licenses.
+ * See COPYRIGHT.php for copyright notices and details.
  */
 
 // no direct access
-defined('_JEXEC') or die('Restricted access');
+defined( '_JEXEC' ) or die( 'Restricted access' );
 
 // Make sure the user is authorized to view this page
 $user = & JFactory::getUser();
 if (!$user->authorize( 'com_config', 'manage' )) {
 	$mainframe->redirect('index.php', JText::_('ALERTNOTAUTH'));
 }
-
-// Require the base controller
-require_once (JPATH_COMPONENT.DS.'controller.php');
 
 // Require specific controller if requested
 if($controller = JRequest::getWord('controller', 'application')) {
@@ -38,6 +35,7 @@ if($controller = JRequest::getWord('controller', 'application')) {
 $classname	= 'ConfigController'.ucfirst($controller);
 $controller	= new $classname( );
 
+// What is this for?
 JResponse::setHeader( 'Expires', 'Mon, 26 Jul 1997 05:00:00 GMT', true );
 
 // Perform the Request task
