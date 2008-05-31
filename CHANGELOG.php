822ecf46fc0f45b43b0eaccfd68c26c20d7d64de
willebil <willebil@6f6e1ebd-4c2b-0410-823f-f34bde69bce9>
2008-05-31 09:24:06 +0000

    git-svn-id: http://joomlacode.org/svn/joomla/development/releases/1.5@10368 6f6e1ebd-4c2b-0410-823f-f34bde69bce9

--- CHANGELOG.php
+++ CHANGELOG.php
@@ -42,6 +42,7 @@ $ -> Language fix or change
  # [10864] PHP session.auto_start leads to broken installation and warning messages
  # [11329] If you have a login menu item, Forgot Password and Forgot Username links don't work
  # [10107] Notice: Trying to get property of non-object in section.php on line 449
+ # [10126] External link in breadcrumb
 
 29-May-2008 Wilco Jansen
  # [10942] Menu does not rebuild sublevel on copy/move (causes menu ordering issues)
