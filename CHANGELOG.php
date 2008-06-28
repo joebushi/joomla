aa204b7385f18f40900e850b8e622046d5ba0713
willebil <willebil@6f6e1ebd-4c2b-0410-823f-f34bde69bce9>
2008-06-28 13:20:22 +0000

    git-svn-id: http://joomlacode.org/svn/joomla/development/releases/1.5@10465 6f6e1ebd-4c2b-0410-823f-f34bde69bce9

--- CHANGELOG.php
+++ CHANGELOG.php
@@ -42,6 +42,7 @@ $ -> Language fix or change
  # [9027] Search system use keyword "Search..." if the inputbox empty
  # [11407] error of time display module pool
  # [11464] Contact router broken on SEF and no Itemid
+ # [11562] Help screen key reference for Category Manager needs to be variable
  ! Patches provided during the second Pizza Bug and Fun event (28/29 june)
 
 27-June-2008 Andrew Eddie
