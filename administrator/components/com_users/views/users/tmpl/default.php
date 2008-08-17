4966e7ebde2418bd2541244d86faa2dcfa34d764
hackwar <hackwar@6f6e1ebd-4c2b-0410-823f-f34bde69bce9>
2008-08-17 19:40:17 +0000

    git-svn-id: http://joomlacode.org/svn/joomla/development/branches/hackwar@10673 6f6e1ebd-4c2b-0410-823f-f34bde69bce9

--- administrator/components/com_users/views/users/tmpl/default.php
+++ administrator/components/com_users/views/users/tmpl/default.php
@@ -6,13 +6,13 @@
 	JToolBarHelper::addNew('addgroup', 'New Group');
 	JToolBarHelper::help( 'screen.users' );
 ?>
-<fieldset class="col width-40">
+<fieldset class="col width-40" style="width:35%;float:left;">
 <legend>Groups &amp; Users</legend>
 <div id="mytree"></div>
 <?php
 echo $this->getTree(true);
 ?>
 </fieldset>
-<fieldset id="detailuser" class="col width-60">
+<fieldset id="detailuser" class="col width-60" style="width:60%;float:right;">
 <legend>Details</legend>
 </fieldset>
