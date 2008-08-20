d855dfaed200f84721698cb9b0f4161c5867d813
plamendp <plamendp@6f6e1ebd-4c2b-0410-823f-f34bde69bce9>
2008-08-20 18:10:30 +0000

    git-svn-id: http://joomlacode.org/svn/joomla/development/branches/joda@10698 6f6e1ebd-4c2b-0410-823f-f34bde69bce9

---
+++ administrator/components/com_config/views/application/tmpl/config_joda_general.php
@@ -0,0 +1,10 @@
+<fieldset class="adminform">
+	<legend><?php echo JText::_( 'General Options' ); ?></legend>
+	<table class="admintable" cellspacing="5">
+		<tbody>
+		<tr>
+            <td>GENERAL OPTIONS AREA</td>
+		</tr>
+		</tbody>
+	</table>
+</fieldset>
