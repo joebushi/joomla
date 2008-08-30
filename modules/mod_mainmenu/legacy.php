c4438d1ae84353282ecbd02073fdd1c2f33e4e95
willebil <willebil@6f6e1ebd-4c2b-0410-823f-f34bde69bce9>
2008-08-30 06:35:08 +0000

    Committed artifact 11621 null
    git-svn-id: http://joomlacode.org/svn/joomla/development/releases/1.5@10856 6f6e1ebd-4c2b-0410-823f-f34bde69bce9

--- modules/mod_mainmenu/legacy.php
+++ modules/mod_mainmenu/legacy.php
@@ -82,7 +82,7 @@ function mosGetMenuLink($mitem, $level = 0, & $params, $open = null)
 	if ($params->get('full_active_id'))
 	{
 		// support for `active_menu` of 'Link - Url' if link is relative
-		if ($id == '' && $mitem->type == 'url' && strpos('http', $mitem->link) === false) {
+		if ($id == '' && $mitem->type == 'url' && strpos($mitem->link, 'http') === false) {
 			$url = array();
 			if(strpos($mitem->link, '&amp;') !== false)
 			{
