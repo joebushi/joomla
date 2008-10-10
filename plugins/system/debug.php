f1cfc8ac20cb0b94232326f0673560b5fafbad10
eddieajau <eddieajau@6f6e1ebd-4c2b-0410-823f-f34bde69bce9>
2008-10-10 23:35:06 +0000

    Merge from 1.5
    git-svn-id: http://joomlacode.org/svn/joomla/development/branches/refactoring@11018 6f6e1ebd-4c2b-0410-823f-f34bde69bce9

--- plugins/system/debug.php
+++ plugins/system/debug.php
@@ -60,9 +60,11 @@ class  plgSystemDebug extends JPlugin
 
 		ob_start();
 		echo '<div id="system-debug" class="profiler">';
-		echo '<h4>'.JText::_( 'Profile Information' ).'</h4>';
-		foreach ( $profiler->getBuffer() as $mark ) {
-			echo '<div>'.$mark.'</div>';
+		if ($this->params->get('profile', 1)) {
+			echo '<h4>'.JText::_( 'Profile Information' ).'</h4>';
+			foreach ( $profiler->getBuffer() as $mark ) {
+				echo '<div>'.$mark.'</div>';
+			}
 		}
 
 		if ($this->params->get('memory', 1)) {
@@ -99,13 +101,29 @@ class  plgSystemDebug extends JPlugin
 				}
 				echo '</ol>';
 			}
+
+			if(isset($database))
+			{
+				echo '<h4>'.JText::sprintf( 'Legacy Queries logged',  $database->getTicker() ).'</h4>';
+				echo '<ol>';
+
+					foreach ($database->getLog() as $k=>$sql)
+					{
+						$geshi->set_source($sql);
+						$text = $geshi->parse_code();
+						$text = preg_replace($newlineKeywords, '<br />&nbsp;&nbsp;\\0', $text);
+						echo '<li>'.$text.'</li>';
+					}
+
+				echo '</ol>';
+			}
 		}
 
-		if ($this->params->get('language', 1))
+		$lang = &JFactory::getLanguage();
+		if ($this->params->get('language_files', 1))
 		{
 			echo '<h4>'.JText::_( 'Language Files Loaded' ).'</h4>';
 			echo '<ul>';
-			$lang = &JFactory::getLanguage();
 			$extensions	= $lang->getPaths();
 			foreach ( $extensions as $extension => $files)
 			{
@@ -115,8 +133,11 @@ class  plgSystemDebug extends JPlugin
 				}
 			}
 			echo '</ul>';
+		}
 
-			echo '<h4>'.JText::_( 'Untranslated strings' ).'</h4>';
+		$langStrings = $this->params->get('language_strings', -1);
+		if ($langStrings < 0 OR $langStrings == 1) {
+			echo '<h4>'.JText::_( 'Untranslated Strings Diagnostic' ).'</h4>';
 			echo '<pre>';
 			$orphans = $lang->getOrphans();
 			if (count( $orphans ))
@@ -124,12 +145,45 @@ class  plgSystemDebug extends JPlugin
 				ksort( $orphans, SORT_STRING );
 				foreach ($orphans as $key => $occurance) {
 					foreach ( $occurance as $i => $info) {
-						$class	= $info['class'];
-						$func	= $info['function'];
-						$file	= $info['file'];
-						$line	= $info['line'];
+						$class	= @$info['class'];
+						$func	= @$info['function'];
+						$file	= @$info['file'];
+						$line	= @$info['line'];
 						echo strtoupper( $key )."\t$class::$func()\t[$file:$line]\n";
 					}
+				}
+			}
+			else {
+				echo JText::_( 'None' );
+			}
+			echo '</pre>';
+		}
+		if ($langStrings < 0 OR $langStrings == 2) {
+			echo '<h4>'.JText::_( 'Untranslated Strings Designer' ).'</h4>';
+			echo '<pre>';
+			$orphans = $lang->getOrphans();
+			if (count( $orphans ))
+			{
+				ksort( $orphans, SORT_STRING );
+				$guesses = array();
+				foreach ($orphans as $key => $occurance) {
+					if (is_array( $occurance ) AND isset( $occurance[0] )) {
+						$info = &$occurance[0];
+						$file = @$info['file'];
+						if (!isset( $guesses[$file] )) {
+							$guesses[$file] = array();
+						}
+
+						$guess = str_replace( '_', ' ', $info['string'] );
+						if ($strip = $this->params->get('language_prefix')) {
+							$guess = trim( preg_replace( chr(1).'^'.$strip.chr(1), '', $guess ) );
+						}
+						$guesses[$file][] = trim( strtoupper( $key ) ).'='.$guess;
+					}
+				}
+				foreach ($guesses as $file => $keys) {
+					echo "\n\n# ".($file ? $file : JText::_( 'Unknown file' ))."\n\n";
+					echo implode( "\n", $keys );
 				}
 			}
 			else {
