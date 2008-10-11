fa303c96f5d6e933afbfe3fa9473c7aeb477ce78
ircmaxell <ircmaxell@6f6e1ebd-4c2b-0410-823f-f34bde69bce9>
2008-10-11 21:39:15 +0000

    Added TODO file...
    git-svn-id: http://joomlacode.org/svn/joomla/development/branches/anthony@11039 6f6e1ebd-4c2b-0410-823f-f34bde69bce9

---
+++ TODO.php
@@ -0,0 +1,38 @@
+<?php
+/**
+* @version		$Id: CHANGELOG.php 10950 2008-09-23 02:50:50Z ian $
+* @package		Joomla
+* @copyright	Copyright (C) 2005 - 2008 Open Source Matters. All rights reserved.
+* @license		GNU/GPL, see LICENSE.php
+* Joomla! is free software. This version may have been modified pursuant
+* to the GNU General Public License, and as distributed it includes or
+* is derivative of works licensed under the GNU General Public License or
+* other free or open source software licenses.
+* See COPYRIGHT.php for copyright notices and details.
+*/
+
+// no direct access
+defined( '_JEXEC' ) or die( 'Restricted access' );
+?>
+TODO:
+
+1. Implement Interfaces For:
+	***JDatabase*** Done
+	JCacheStorage
+	JSessionStorage
+	JFileStorage
+	JParameterElement
+	JDocument
+
+
+2. Remove php4 special code
+	***Remove Compat files for &lt; 5.2.x*** Done
+	Remove all additional php4 code
+
+3. Public/Private/Protected Methods
+
+4. Abstract classes
+
+5. Replace is_a($obj, 'ClassName') with ($class INSTANCEOF ClassName)
+
+
