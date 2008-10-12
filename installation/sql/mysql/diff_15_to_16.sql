f0a8a66f1348bff9ff8dd351032e4e66fd28496c
pentacle <pentacle@6f6e1ebd-4c2b-0410-823f-f34bde69bce9>
2008-10-12 11:31:55 +0000

    changed all table prefixes to jos_ for now added a few missing semicolons
    git-svn-id: http://joomlacode.org/svn/joomla/development/branches/refactoring@11042 6f6e1ebd-4c2b-0410-823f-f34bde69bce9

--- installation/sql/mysql/diff_15_to_16.sql
+++ installation/sql/mysql/diff_15_to_16.sql
@@ -10,17 +10,17 @@ ALTER TABLE `jos_core_acl_groups_aro_map`
 --
 
 ALTER TABLE `jos_weblinks`
- CHANGE `published` `state` TINYINT( 1 ) NOT NULL DEFAULT '0'
+ CHANGE `published` `state` TINYINT( 1 ) NOT NULL DEFAULT '0';
 
 -- 2008-10-10
 
-DROP TABLE `jos_groups`
+DROP TABLE `jos_groups`;
 
 --
--- Table structure for table `#__core_acl_acl`
+-- Table structure for table `jos_core_acl_acl`
 --
 
-CREATE TABLE IF NOT EXISTS `#__core_acl_acl` (
+CREATE TABLE IF NOT EXISTS `jos_core_acl_acl` (
   `id` int(10) unsigned NOT NULL auto_increment,
   `section_value` varchar(100) NOT NULL default 'system',
   `allow` int(1) unsigned NOT NULL default '0',
@@ -37,32 +37,32 @@ CREATE TABLE IF NOT EXISTS `#__core_acl_acl` (
 -- --------------------------------------------------------
 
 --
--- Table structure for table `#__core_acl_acl_sections`
+-- Table structure for table `jos_core_acl_acl_sections`
 --
 
 
-CREATE TABLE IF NOT EXISTS `#__core_acl_acl_sections` (
+CREATE TABLE IF NOT EXISTS `jos_core_acl_acl_sections` (
   `id` int(10) unsigned NOT NULL auto_increment,
   `value` varchar(100) NOT NULL default '',
   `order_value` int(11) NOT NULL default '0',
   `name` varchar(230) NOT NULL default '',
   `hidden` int(1) unsigned NOT NULL default '0',
   PRIMARY KEY  (`id`),
-  UNIQUE KEY `#__core_acl_value_acl_sections` (`value`),
+  UNIQUE KEY `jos_core_acl_value_acl_sections` (`value`),
   KEY `core_acl_hidden_acl_sections` (`hidden`)
 ) ENGINE=MyISAM CHARACTER SET `utf8`;
 
-INSERT IGNORE INTO `#__core_acl_acl_sections` VALUES (1, 'system', 1, 'System', 0);
-INSERT IGNORE INTO `#__core_acl_acl_sections` VALUES (2, 'user', 2, 'User', 0);
+INSERT IGNORE INTO `jos_core_acl_acl_sections` VALUES (1, 'system', 1, 'System', 0);
+INSERT IGNORE INTO `jos_core_acl_acl_sections` VALUES (2, 'user', 2, 'User', 0);
 
 -- --------------------------------------------------------
 
 
 --
--- Table structure for table `#__core_acl_aco`
+-- Table structure for table `jos_core_acl_aco`
 --
 
-CREATE TABLE IF NOT EXISTS `#__core_acl_aco` (
+CREATE TABLE IF NOT EXISTS `jos_core_acl_aco` (
   `id` int(10) unsigned NOT NULL auto_increment,
   `section_value` varchar(100) NOT NULL default '0',
   `value` varchar(100) NOT NULL default '',
@@ -72,7 +72,7 @@ CREATE TABLE IF NOT EXISTS `#__core_acl_aco` (
   `allow_axos` int(1) unsigned NOT NULL default '0',
   `note` mediumtext,
   PRIMARY KEY  (`id`),
-  UNIQUE KEY `#__core_acl_section_value_aco` (`section_value`,`value`),
+  UNIQUE KEY `jos_core_acl_section_value_aco` (`section_value`,`value`),
   KEY `core_acl_hidden_aco` (`hidden`),
   KEY `core_acl_axo_section` (`allow_axos`,`section_value`)
 ) ENGINE=MyISAM CHARACTER SET `utf8`;
@@ -80,10 +80,10 @@ CREATE TABLE IF NOT EXISTS `#__core_acl_aco` (
 -- --------------------------------------------------------
 
 --
--- Table structure for table `#__core_acl_aco_map`
+-- Table structure for table `jos_core_acl_aco_map`
 --
 
-CREATE TABLE IF NOT EXISTS `#__core_acl_aco_map` (
+CREATE TABLE IF NOT EXISTS `jos_core_acl_aco_map` (
   `acl_id` int(10) unsigned NOT NULL default '0',
   `section_value` varchar(100) NOT NULL default '0',
   `value` varchar(100) NOT NULL default '',
@@ -93,10 +93,10 @@ CREATE TABLE IF NOT EXISTS `#__core_acl_aco_map` (
 -- --------------------------------------------------------
 
 --
--- Table structure for table `#__core_acl_aco_sections`
+-- Table structure for table `jos_core_acl_aco_sections`
 --
 
-CREATE TABLE IF NOT EXISTS `#__core_acl_aco_sections` (
+CREATE TABLE IF NOT EXISTS `jos_core_acl_aco_sections` (
   `id` int(10) unsigned NOT NULL auto_increment,
   `value` varchar(100) NOT NULL default '',
   `order_value` int(11) NOT NULL default '0',
@@ -110,17 +110,17 @@ CREATE TABLE IF NOT EXISTS `#__core_acl_aco_sections` (
 -- --------------------------------------------------------
 
 --
--- Table structure for table `#__core_acl_aro_map`
+-- Table structure for table `jos_core_acl_aro_map`
 --
 
-CREATE TABLE IF NOT EXISTS `#__core_acl_aro_map` (
+CREATE TABLE IF NOT EXISTS `jos_core_acl_aro_map` (
   `acl_id` int(10) unsigned NOT NULL default '0',
   `section_value` varchar(100) NOT NULL default '0',
   `value` varchar(100) NOT NULL default '',
   PRIMARY KEY  (`acl_id`,`section_value`,`value`)
 ) ENGINE=MyISAM CHARACTER SET `utf8`;
 
-CREATE TABLE IF NOT EXISTS  `#__core_acl_aro_groups_map` (
+CREATE TABLE IF NOT EXISTS  `jos_core_acl_aro_groups_map` (
   `acl_id` int(11) NOT NULL default '0',
   `group_id` int(11) NOT NULL default '0',
   PRIMARY KEY  (`acl_id`,`group_id`)
@@ -129,10 +129,10 @@ CREATE TABLE IF NOT EXISTS  `#__core_acl_aro_groups_map` (
 -- --------------------------------------------------------
 
 --
--- Table structure for table `#__core_acl_axo`
+-- Table structure for table `jos_core_acl_axo`
 --
 
-CREATE TABLE IF NOT EXISTS `#__core_acl_axo` (
+CREATE TABLE IF NOT EXISTS `jos_core_acl_axo` (
   `id` int(10) unsigned NOT NULL auto_increment,
   `section_value` varchar(100) NOT NULL default '0',
   `value` varchar(100) NOT NULL default '',
@@ -140,17 +140,17 @@ CREATE TABLE IF NOT EXISTS `#__core_acl_axo` (
   `name` varchar(255) NOT NULL default '',
   `hidden` int(1) unsigned NOT NULL default '0',
   PRIMARY KEY  (`id`),
-  UNIQUE KEY `#__core_acl_section_value_value_axo` (`section_value`,`value`),
+  UNIQUE KEY `jos_core_acl_section_value_value_axo` (`section_value`,`value`),
   KEY `core_acl_hidden_axo` (`hidden`)
 ) ENGINE=MyISAM CHARACTER SET `utf8`;
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `#__core_acl_axo_groups`
+-- Table structure for table `jos_core_acl_axo_groups`
 --
 
-CREATE TABLE IF NOT EXISTS `#__core_acl_axo_groups` (
+CREATE TABLE IF NOT EXISTS `jos_core_acl_axo_groups` (
   `id` int(10) unsigned NOT NULL auto_increment,
   `parent_id` int(10) unsigned NOT NULL default '0',
   `lft` int(10) unsigned NOT NULL default '0',
@@ -158,23 +158,23 @@ CREATE TABLE IF NOT EXISTS `#__core_acl_axo_groups` (
   `name` varchar(255) NOT NULL default '',
   `value` int(10) NOT NULL default '0',
   PRIMARY KEY  (`id`,`value`),
-  UNIQUE KEY `#__core_acl_value_axo_groups` (`value`),
+  UNIQUE KEY `jos_core_acl_value_axo_groups` (`value`),
   KEY `core_acl_parent_id_axo_groups` (`parent_id`),
   KEY `core_acl_lft_rgt_axo_groups` (`lft`,`rgt`)
 ) ENGINE=MyISAM CHARACTER SET `utf8`;
 
-INSERT IGNORE INTO `#__core_acl_axo_groups` VALUES (1, 0, 1, 8, 'ROOT', -1);
-INSERT IGNORE INTO `#__core_acl_axo_groups` VALUES (2, 1, 2, 3, 'Public', '0');
-INSERT IGNORE INTO `#__core_acl_axo_groups` VALUES (3, 1, 4, 5, 'Registered', '1');
-INSERT IGNORE INTO `#__core_acl_axo_groups` VALUES (4, 1, 6, 7, 'Special', '2');
+INSERT IGNORE INTO `jos_core_acl_axo_groups` VALUES (1, 0, 1, 8, 'ROOT', -1);
+INSERT IGNORE INTO `jos_core_acl_axo_groups` VALUES (2, 1, 2, 3, 'Public', '0');
+INSERT IGNORE INTO `jos_core_acl_axo_groups` VALUES (3, 1, 4, 5, 'Registered', '1');
+INSERT IGNORE INTO `jos_core_acl_axo_groups` VALUES (4, 1, 6, 7, 'Special', '2');
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `#__core_acl_axo_groups_map`
+-- Table structure for table `jos_core_acl_axo_groups_map`
 --
 
-CREATE TABLE IF NOT EXISTS `#__core_acl_axo_groups_map` (
+CREATE TABLE IF NOT EXISTS `jos_core_acl_axo_groups_map` (
   `acl_id` int(10) unsigned NOT NULL default '0',
   `group_id` int(10) unsigned NOT NULL default '0',
   PRIMARY KEY  (`acl_id`,`group_id`)
@@ -183,10 +183,10 @@ CREATE TABLE IF NOT EXISTS `#__core_acl_axo_groups_map` (
 -- --------------------------------------------------------
 
 --
--- Table structure for table `#__core_acl_axo_map`
+-- Table structure for table `jos_core_acl_axo_map`
 --
 
-CREATE TABLE IF NOT EXISTS `#__core_acl_axo_map` (
+CREATE TABLE IF NOT EXISTS `jos_core_acl_axo_map` (
   `acl_id` int(10) unsigned NOT NULL default '0',
   `section_value` varchar(100) NOT NULL default '0',
   `value` varchar(100) NOT NULL default '',
@@ -196,31 +196,31 @@ CREATE TABLE IF NOT EXISTS `#__core_acl_axo_map` (
 -- --------------------------------------------------------
 
 --
--- Table structure for table `#__core_acl_axo_sections`
+-- Table structure for table `jos_core_acl_axo_sections`
 --
 
-CREATE TABLE IF NOT EXISTS `#__core_acl_axo_sections` (
+CREATE TABLE IF NOT EXISTS `jos_core_acl_axo_sections` (
   `id` int(10) unsigned NOT NULL auto_increment,
   `value` varchar(100) NOT NULL default '',
   `order_value` int(11) NOT NULL default '0',
   `name` varchar(230) NOT NULL default '',
   `hidden` int(1) unsigned NOT NULL default '0',
   PRIMARY KEY  (`id`),
-  UNIQUE KEY `#__core_acl_value_axo_sections` (`value`),
+  UNIQUE KEY `jos_core_acl_value_axo_sections` (`value`),
   KEY `core_acl_hidden_axo_sections` (`hidden`)
 ) ENGINE=MyISAM CHARACTER SET `utf8`;
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `#__core_acl_groups_axo_map`
+-- Table structure for table `jos_core_acl_groups_axo_map`
 --
 
-CREATE TABLE IF NOT EXISTS `#__core_acl_groups_axo_map` (
+CREATE TABLE IF NOT EXISTS `jos_core_acl_groups_axo_map` (
   `group_id` int(10) unsigned NOT NULL default '0',
   `axo_id` int(10) unsigned NOT NULL default '0',
   PRIMARY KEY  (`group_id`,`axo_id`),
-  KEY `#__core_acl_axo_id` (`axo_id`),
+  KEY `jos_core_acl_axo_id` (`axo_id`),
   INDEX `group_id_axo_id_groups_axo_map` USING BTREE(`axo_id`, `group_id`),
   INDEX `aro_id_group_id_groups_axo_map` USING BTREE(`group_id`, `axo_id`)
 ) ENGINE=MyISAM CHARACTER SET `utf8`;
