1be0e07bdcbe8d35b3b71e27c2a497f5429b87e4
eddieajau <eddieajau@6f6e1ebd-4c2b-0410-823f-f34bde69bce9>
2008-10-13 10:33:50 +0000

    Fix validation error
    git-svn-id: http://joomlacode.org/svn/joomla/development/branches/refactoring@11080 6f6e1ebd-4c2b-0410-823f-f34bde69bce9

--- templates/system/error.php
+++ templates/system/error.php
@@ -34,11 +34,9 @@ defined( '_JEXEC' ) or die( 'Restricted access' );
 					<li><?php echo JText::_('An error has occurred while processing your request.'); ?></li>
 				</ol>
 			<p><strong><?php echo JText::_('Please try one of the following pages:'); ?></strong></p>
-			<p>
-				<ul>
-					<li><a href="<?php echo $this->baseurl; ?>/index.php" title="<?php echo JText::_('Go to the home page'); ?>"><?php echo JText::_('Home Page'); ?></a></li>
-				</ul>
-			</p>
+			<ul>
+				<li><a href="<?php echo $this->baseurl; ?>/index.php" title="<?php echo JText::_('Go to the home page'); ?>"><?php echo JText::_('Home Page'); ?></a></li>
+			</ul>
 			<p><?php echo JText::_('If difficulties persist, please contact the system administrator of this site.'); ?></p>
 			<div id="techinfo">
 			<p><?php echo $this->error->message; ?></p>
