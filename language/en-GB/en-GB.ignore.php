97fd938fc50ef8e22e40be346ad0dfcf827e2235
ian <ian@6f6e1ebd-4c2b-0410-823f-f34bde69bce9>
2009-04-04 17:45:23 +0000

    Set keywords
    git-svn-id: http://joomlacode.org/svn/joomla/development/releases/1.5@11746 6f6e1ebd-4c2b-0410-823f-f34bde69bce9

--- language/en-GB/en-GB.ignore.php
+++ language/en-GB/en-GB.ignore.php
@@ -1,6 +1,6 @@
 <?php
 /**
-* @version		$Id$
+* @version		$Id: en-GB.ignore.php 10381 2008-06-01 03:35:53Z pasamio $
 * @package		Joomla
 * @copyright	Copyright (C) 2005 - 2008 Open Source Matters. All rights reserved.
 * @license		GNU/GPL, see LICENSE.php
