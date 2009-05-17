7e651130d4531c1b46549990c32432d7c445f6ec
erdsiger <erdsiger@6f6e1ebd-4c2b-0410-823f-f34bde69bce9>
2009-05-17 13:37:33 +0000

    Replaced Contacts search plugin.
    git-svn-id: http://joomlacode.org/svn/joomla/development/trunk@11827 6f6e1ebd-4c2b-0410-823f-f34bde69bce9

--- plugins/search/contacts.php
+++ plugins/search/contacts.php
@@ -1,19 +1,24 @@
 <?php
 /**
  * @version		$Id$
- * @package		Joomla
  * @copyright	Copyright (C) 2005 - 2009 Open Source Matters, Inc. All rights reserved.
  * @license		GNU General Public License, see LICENSE.php
-  */
+ */
 
-// no direct access
-defined( '_JEXEC' ) or die( 'Restricted access' );
+// Check to ensure this file is included in Joomla!
+defined('_JEXEC') or die('Restricted access');
 
 jimport('joomla.plugin.plugin');
 
+/**
+ * Contacts Search class
+ * 
+ * @package		Joomla.Site
+ * @subpackage	Contacts
+ */
 class plgSearchContacts extends JPlugin
 {
-	protected $areas = array('contacts' => 'Contacts');
+	protected $areas = array('contacts'=>'Contacts');
 
 	public function __construct(&$subject, $options = array())
 	{
@@ -38,73 +43,67 @@ class plgSearchContacts extends JPlugin
 	* @param string mathcing option, exact|any|all
 	* @param string ordering option, newest|oldest|popular|alpha|category
 	*/
-	public function onSearch( $text, $phrase='', $ordering='', $areas=null )
+	public function onSearch($text, $phrase='', $ordering='', $areas=null)
 	{
-		$db		= JFactory::getDBO();
-		$user	= JFactory::getUser();
+		$db = JFactory::getDBO();
+		$user = JFactory::getUser();
 
-		if (is_array( $areas )) {
-			if (!array_intersect( $areas, array_keys( $this->areas ) )) {
+		if (is_array($areas)) {
+			if (!array_intersect($areas, array_keys($this->areas))) {
 				return array();
 			}
 		}
 
-		$limit = $this->params->def( 'search_limit', 50 );
+		$limit = $this->params->def('search_limit', 50);
 
-		$text = trim( $text );
+		$text = trim($text);
 		if ($text == '') {
 			return array();
 		}
 
-		$section = JText::_( 'Contact' );
+		$section = JText::_('Contacts');
 
-		switch ( $ordering ) {
+		switch ($ordering) {
 			case 'alpha':
-				$order = 'a.name ASC';
+				$order = 'c.name ASC';
 				break;
 
 			case 'category':
-				$order = 'b.title ASC, a.name ASC';
+				$order = 'cat.title ASC, c.name ASC';
 				break;
 
 			case 'popular':
 			case 'newest':
 			case 'oldest':
 			default:
-				$order = 'a.name DESC';
+				$order = 'c.name DESC';
 		}
 
-		$text	= $db->Quote( '%'.$db->getEscaped( $text, true ).'%', false );
-		$query	= 'SELECT a.name AS title, "" AS created,'
-		. ' CASE WHEN CHAR_LENGTH(a.alias) THEN CONCAT_WS(\':\', a.id, a.alias) ELSE a.id END as slug, '
-		. ' CASE WHEN CHAR_LENGTH(b.alias) THEN CONCAT_WS(\':\', b.id, b.alias) ELSE b.id END AS catslug, '
-		. ' CONCAT_WS( ", ", a.name, a.con_position, a.misc ) AS text,'
-		. ' CONCAT_WS( " / ", '.$db->Quote($section).', b.title ) AS section,'
+		$text = $db->Quote( '%'.$db->getEscaped( $text, true ).'%', false );
+
+		$query = 'SELECT DISTINCT c.name AS title, "" AS created,'
+		. ' CASE WHEN CHAR_LENGTH(c.alias) THEN CONCAT_WS(\':\', c.id, c.alias) ELSE c.id END as slug, '
+		. ' CASE WHEN CHAR_LENGTH(cat.alias) THEN CONCAT_WS(\':\', cat.id, cat.alias) ELSE cat.id END AS catslug, '
+		. ' "" AS text,'
+		. ' CONCAT_WS( " / ", '.$db->Quote($section).', cat.title ) AS section,'
 		. ' "2" AS browsernav'
-		. ' FROM #__contact_details AS a'
-		. ' INNER JOIN #__categories AS b ON b.id = a.catid'
-		. ' WHERE ( a.name LIKE '.$text
-		. ' OR a.misc LIKE '.$text
-		. ' OR a.con_position LIKE '.$text
-		. ' OR a.address LIKE '.$text
-		. ' OR a.suburb LIKE '.$text
-		. ' OR a.state LIKE '.$text
-		. ' OR a.country LIKE '.$text
-		. ' OR a.postcode LIKE '.$text
-		. ' OR a.telephone LIKE '.$text
-		. ' OR a.fax LIKE '.$text.' )'
-		. ' AND a.published = 1'
-		. ' AND b.published = 1'
-		. ' AND a.access <= '.(int) $user->get( 'aid' )
-		. ' AND b.access <= '.(int) $user->get( 'aid' )
-		. ' GROUP BY a.id'
+		. ' FROM #__contacts_contacts AS c'
+		. ' LEFT JOIN #__contacts_con_cat_map AS map ON map.contact_id = c.id '
+		. ' LEFT JOIN #__categories AS cat ON cat.id = map.category_id '
+		. ' LEFT JOIN #__contacts_details AS d ON d.contact_id = c.id '
+		. ' WHERE ( d.data LIKE ' . $text . ' OR c.name LIKE ' . $text . ' ) '
+		. ' AND c.published = 1'
+		. ' AND cat.published = 1'
+		. ' AND c.access <= '.(int) $user->get( 'aid' )
+		. ' AND cat.access <= '.(int) $user->get( 'aid' )
+		. ' GROUP BY c.id'
 		. ' ORDER BY '. $order
 		;
-		$db->setQuery( $query, 0, $limit );
+		$db->setQuery($query, 0, $limit);
 		$rows = $db->loadObjectList();
 
 		foreach($rows as $key => $row) {
-			$rows[$key]->href = 'index.php?option=com_contact&view=contact&id='.$row->slug.'&catid='.$row->catslug;
+			$rows[$key]->href = 'index.php?option=com_contacts&view=contact&id='.$row->slug;
 		}
 
 		return $rows;
