ce5a6483357e862753d345de71aab21be6c62a3f
louis <louis@6f6e1ebd-4c2b-0410-823f-f34bde69bce9>
2009-06-01 23:09:42 +0000

    Adding basic scaffolding for the new menu manager code.
    git-svn-id: http://joomlacode.org/svn/joomla/development/branches/menus@11959 6f6e1ebd-4c2b-0410-823f-f34bde69bce9

---
+++ administrator/components/com_menus/helpers/html/menu.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * @version		$Id$
+ * @package		Joomla.Administrator
+ * @subpackage	com_menus
+ * @copyright	Copyright (C) 2005 - 2009 Open Source Matters, Inc. All rights reserved.
+ * @license		GNU General Public License version 2 or later; see LICENSE.txt
+ */
+
+defined('_JEXEC') or die;
+
+/**
+ * HTML Helper Class for Menus
+ *
+ * @package		Joomla.Administrator
+ * @subpackage	com_menus
+ */
+class JHtmlMenu
+{
+}
---
+++ administrator/components/com_menus/views/items/tmpl/default.php
@@ -0,0 +1,54 @@
+<?php
+/**
+ * @version		$Id$
+ * @package		Joomla.Administrator
+ * @subpackage	com_menus
+ * @copyright	Copyright (C) 2005 - 2009 Open Source Matters, Inc. All rights reserved.
+ * @license		GNU General Public License version 2 or later; see LICENSE.txt
+ */
+
+defined('_JEXEC') or die;
+
+// Include the component HTML helpers.
+JHtml::addIncludePath(JPATH_COMPONENT.'/helpers/html');
+
+// Load the tooltip behavior.
+JHtml::_('behavior.tooltip');
+
+// Load the default stylesheet.
+JHtml::stylesheet('default.css', 'administrator/components/com_menus/media/css/');
+
+// Build the toolbar.
+$this->buildDefaultToolBar();
+?>
+<form action="<?php echo JRoute::_('index.php?option=com_menus&view=items');?>" method="post" name="adminForm">
+	<fieldset class="filter">
+		<div class="left">
+			<label for="search"><?php echo JText::_('JSearch_Filter'); ?>:</label>
+			<input type="text" name="filter_search" id="search" value="<?php echo $this->state->get('filter.search'); ?>" size="60" title="<?php echo JText::_('Members_Search_in_title'); ?>" />
+			<button type="submit"><?php echo JText::_('JSearch_Filter_Submit'); ?></button>
+			<button type="button" onclick="$('search').value='';this.form.submit();"><?php echo JText::_('JSearch_Filter_Clear'); ?></button>
+		</div>
+	</fieldset>
+	<table class="adminlist">
+		<thead>
+			<tr>
+			</tr>
+		</thead>
+		<tfoot>
+			<tr>
+				<td colspan="15">
+					<?php echo $this->pagination->getListFooter(); ?>
+				</td>
+			</tr>
+		</tfoot>
+		<tbody>
+		</tbody>
+	</table>
+
+	<input type="hidden" name="task" value="" />
+	<input type="hidden" name="boxchecked" value="0" />
+	<input type="hidden" name="filter_order" value="<?php echo $this->state->get('list.ordering'); ?>" />
+	<input type="hidden" name="filter_order_Dir" value="<?php echo $this->state->get('list.direction'); ?>" />
+	<?php echo JHtml::_('form.token'); ?>
+</form>
