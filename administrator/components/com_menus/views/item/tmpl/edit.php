17b4f97c133d3e1f792acebeb7c71528f9144aef
dextercowley <dextercowley@6f6e1ebd-4c2b-0410-823f-f34bde69bce9>
2009-09-21 03:56:53 +0000

    Update trunk merge through trunk revision 12804, plus fix menu item columns.
    git-svn-id: http://joomlacode.org/svn/joomla/development/branches/dextercowley@12805 6f6e1ebd-4c2b-0410-823f-f34bde69bce9

--- administrator/components/com_menus/views/item/tmpl/edit.php
+++ administrator/components/com_menus/views/item/tmpl/edit.php
@@ -32,7 +32,7 @@ JHtml::_('behavior.formvalidation');
 
 <form action="<?php JRoute::_('index.php?option=com_menus'); ?>" method="post" name="adminForm" id="item-form" class="form-validate">
 
-<div class="width-45">
+<div class="col width-50">
 	<fieldset>
 		<legend><?php echo JText::_('Menus_Item_Details');?></legend>
 		
@@ -57,7 +57,7 @@ JHtml::_('behavior.formvalidation');
 	</fieldset>
 </div>
 
-<div class="width-45" style="clear:right;">
+<div class="col width-50" style="clear:right;">
 	<div  style="float:left">		
 			<?php echo $pane->startPane('menu-pane'); ?>			
 				<?php echo $pane->startPanel(JText::_('Menu_Advanced_Menu_Options'), 'advanced-options'); ?>
