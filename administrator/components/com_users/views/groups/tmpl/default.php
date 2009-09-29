b6daf789e5b91360f5f1a5d9a26a44ddc8f7c2b2
eddieajau <eddieajau@6f6e1ebd-4c2b-0410-823f-f34bde69bce9>
2009-09-29 06:37:40 +0000

    Added group ID column.
    git-svn-id: http://joomlacode.org/svn/joomla/development/branches/acl2@12910 6f6e1ebd-4c2b-0410-823f-f34bde69bce9

--- administrator/components/com_users/views/groups/tmpl/default.php
+++ administrator/components/com_users/views/groups/tmpl/default.php
@@ -40,8 +40,11 @@ JHtml::stylesheet('default.css', 'administrator/components/com_users/media/css/'
 				<th width="10%">
 					<?php echo JText::_('Users_Heading_Users_in_group'); ?>
 				</th>
+				<th width="5%">
+					<?php echo JText::_('JGrid_Heading_ID'); ?>
+				</th>
 				<th width="40%">
-					<?php //echo JText::_('Users_Heading_Group_Actions'); ?>
+					&nbsp;
 				</th>
 			</tr>
 		</thead>
@@ -67,18 +70,11 @@ JHtml::stylesheet('default.css', 'administrator/components/com_users/media/css/'
 				<td align="center">
 					<?php echo $item->user_count ? $item->user_count : ''; ?>
 				</td>
+				<td align="center">
+					<?php echo $item->id; ?>
+				</td>
 				<td>
 					&nbsp;
-					<?php
-					/*
-					// This needs to be tidied up or put in a modal
-					$actions = explode(',', $item->actions);
-					for ($i = 0, $t = count($actions); $i < $t; $i++) {
-						echo JText::_($actions[$i]);
-						echo '<br />';
-					}
-					*/
-					?>
 				</td>
 			</tr>
 			<?php
