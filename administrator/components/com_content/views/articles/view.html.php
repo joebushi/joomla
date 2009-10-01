3c7bd930239fb69fa236aa55147b9dfab5fed611
eddieajau <eddieajau@6f6e1ebd-4c2b-0410-823f-f34bde69bce9>
2009-10-01 03:35:41 +0000

    Fix docblock.
    git-svn-id: http://joomlacode.org/svn/joomla/development/branches/acl2@12956 6f6e1ebd-4c2b-0410-823f-f34bde69bce9

--- administrator/components/com_content/views/articles/view.html.php
+++ administrator/components/com_content/views/articles/view.html.php
@@ -45,21 +45,19 @@ class ContentViewArticles extends JView
 
 	/**
 	 * Display the toolbar
-	 *
-	 * @access	private
 	 */
 	protected function _setToolbar()
 	{
 		$state = $this->get('State');
 		JToolBarHelper::title(JText::_('Content_Articles_Title'), 'article.png');
-		JToolBarHelper::custom('article.edit', 'new.png', 'new_f2.png', 'New', false);	
+		JToolBarHelper::custom('article.edit', 'new.png', 'new_f2.png', 'New', false);
 		JToolBarHelper::custom('article.edit', 'edit.png', 'edit_f2.png', 'Edit', true);
 		JToolBarHelper::divider();
 		JToolBarHelper::custom('articles.publish', 'publish.png', 'publish_f2.png', 'Publish', true);
 		JToolBarHelper::custom('articles.unpublish', 'unpublish.png', 'unpublish_f2.png', 'Unpublish', true);
 		if ($state->get('filter.published') != -1) {
 			JToolBarHelper::archiveList('articles.archive');
-		}		
+		}
 		if ($state->get('filter.published') == -2) {
 			JToolBarHelper::deleteList('', 'articles.delete');
 		}
@@ -68,7 +66,7 @@ class ContentViewArticles extends JView
 		}
 		JToolBarHelper::divider();
 		JToolBarHelper::preferences('com_content');
-		JToolBarHelper::divider();		
+		JToolBarHelper::divider();
 		JToolBarHelper::help('screen.content.articles');
 	}
 }
