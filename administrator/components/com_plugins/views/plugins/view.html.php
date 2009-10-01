46328219a0e25a9874dc1a6fddd9db36d5f99bd7
eddieajau <eddieajau@6f6e1ebd-4c2b-0410-823f-f34bde69bce9>
2009-10-01 03:52:21 +0000

    Hook in component access controls.
    git-svn-id: http://joomlacode.org/svn/joomla/development/branches/acl2@12963 6f6e1ebd-4c2b-0410-823f-f34bde69bce9

--- administrator/components/com_plugins/views/plugins/view.html.php
+++ administrator/components/com_plugins/views/plugins/view.html.php
@@ -31,14 +31,6 @@ class PluginsViewPlugins extends JView
 
 	function display( $tpl = null )
 	{
-	JToolBarHelper::title( JText::_( 'Plugin Manager' ), 'plugin.png' );
-	JToolBarHelper::editList();
-	JToolBarHelper::publishList();
-	JToolBarHelper::unpublishList();
-	JToolBarHelper::divider();
-	JToolBarHelper::help( 'screen.plugins' );
-	
-	
 		$app	= &JFactory::getApplication();
 		$db		=& JFactory::getDBO();
 
@@ -148,6 +140,22 @@ class PluginsViewPlugins extends JView
 		$this->assignRef('items',		$rows);
 		$this->assignRef('pagination',	$pagination);
 
+		$this->_setToolbar();
 		parent::display($tpl);
+	}
+
+	/**
+	 * Display the toolbar
+	 */
+	protected function _setToolbar()
+	{
+		JToolBarHelper::title( JText::_( 'Plugin Manager' ), 'plugin.png' );
+		JToolBarHelper::editList();
+		JToolBarHelper::publishList();
+		JToolBarHelper::unpublishList();
+		JToolBarHelper::divider();
+		JToolBarHelper::preferences('com_plugins');
+		JToolBarHelper::divider();
+		JToolBarHelper::help( 'screen.plugins' );
 	}
 }
