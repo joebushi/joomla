18c0703acfd1721ab795167a0934c79a18babc86
eddieajau <eddieajau@6f6e1ebd-4c2b-0410-823f-f34bde69bce9>
2009-10-02 05:06:11 +0000

    Bug fix incorrect access check.
    git-svn-id: http://joomlacode.org/svn/joomla/development/branches/acl2@13015 6f6e1ebd-4c2b-0410-823f-f34bde69bce9

--- administrator/components/com_weblinks/weblinks.php
+++ administrator/components/com_weblinks/weblinks.php
@@ -11,7 +11,7 @@
 defined('_JEXEC') or die;
 
 // Access check.
-if (!JFactory::getUser()->authorise('core.manage', 'com_users')) {
+if (!JFactory::getUser()->authorise('core.manage', 'com_weblinks')) {
 	return JError::raiseWarning(404, JText::_('ALERTNOTAUTH'));
 }
 
