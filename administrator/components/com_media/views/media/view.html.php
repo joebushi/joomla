95ba274ef9ed77c212a3009d5fae6b08734df0d5
ian <ian@6f6e1ebd-4c2b-0410-823f-f34bde69bce9>
2009-10-06 05:17:02 +0000

    Beginning of implementation of new FancyUpload
    git-svn-id: http://joomlacode.org/svn/joomla/development/branches/uploader@13073 6f6e1ebd-4c2b-0410-823f-f34bde69bce9

--- administrator/components/com_media/views/media/view.html.php
+++ administrator/components/com_media/views/media/view.html.php
@@ -52,8 +52,12 @@ class MediaViewMedia extends JView
 		JHtml::script('mootree.js');
 		JHtml::stylesheet('mootree.css');
 
-		if ($config->get('enable_flash', 0)) {
-			JHtml::_('behavior.uploader', 'file-upload', array('onAllComplete' => 'function(){ MediaManager.refreshFrame(); }'));
+		if ($config->get('enable_flash', 1)) {
+			JHtml::_('behavior.uploader', 'uploader-flash', 
+				array(
+					'onAllComplete' => 'function(){ MediaManager.refreshFrame(); }',
+				)
+			);
 		}
 
 		if (DS == '\\')
