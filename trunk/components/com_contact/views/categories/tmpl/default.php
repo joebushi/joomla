d1404bf60c4774563fc1a74ce249a938ffb711da
andrea.tarr <andrea.tarr@6f6e1ebd-4c2b-0410-823f-f34bde69bce9>
2009-10-07 22:41:33 +0000

    Fix the link urls on com_contact categories view
    git-svn-id: http://joomlacode.org/svn/joomla/development/branches/dextercowley_2@13096 6f6e1ebd-4c2b-0410-823f-f34bde69bce9

--- trunk/components/com_contact/views/categories/tmpl/default.php
+++ trunk/components/com_contact/views/categories/tmpl/default.php
@@ -44,7 +44,7 @@ $difLevel = 0;
 	?>
 	
 	<li>
-		<span class="jitem-title"><a href="<?php echo ContactRoute::category('index.php?option=com_contact&view=category&id='.$this->escape($item->slug));?>">
+		<span class="jitem-title"><a href="<?php echo ContactRoute::category('index.php?option=com_contact&view=category&catid='.$this->escape($item->slug));?>">
 			<?php echo $this->escape($item->title); ?></a>
 		</span>
 		<?php if ($item->description) : ?>
