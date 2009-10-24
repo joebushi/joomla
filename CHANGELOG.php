2278d3a0ac9cf623cdc8b5267a3460327e1467e6
ian <ian@6f6e1ebd-4c2b-0410-823f-f34bde69bce9>
2009-10-24 02:44:09 +0000

    # [#18246] Tinymce sql correction
    git-svn-id: http://joomlacode.org/svn/joomla/development/releases/1.5@13306 6f6e1ebd-4c2b-0410-823f-f34bde69bce9

--- CHANGELOG.php
+++ CHANGELOG.php
@@ -43,6 +43,7 @@ $ -> Language fix or change
  # [#17259] html not allowed in user confirmation message
  # [#17430] Image Title attribute field has different names
  # [#17964] Email cloaking. Syntax error in email.php. Patch included.
+ # [#18246] Tinymce sql correction
 
 19-Oct-2009 Ian MacLennan
  # [#18297] Setting ftp fields to not autocomplete
