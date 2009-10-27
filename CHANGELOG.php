633efa7689c0eff9cd7a89385d6d503acd38b94c
ian <ian@6f6e1ebd-4c2b-0410-823f-f34bde69bce9>
2009-10-27 02:56:28 +0000

    # [#18050] SEF category links lead to article not found
    git-svn-id: http://joomlacode.org/svn/joomla/development/releases/1.5@13340 6f6e1ebd-4c2b-0410-823f-f34bde69bce9

--- CHANGELOG.php
+++ CHANGELOG.php
@@ -43,6 +43,7 @@ $ -> Language fix or change
  # [#17110] Section/Category List - Table width issue in IE8
  # [#17470] Archive filters don't work with cache on + com_content cache optimization
  # [#17475] Cache unused by core components
+ # [#18050] SEF category links lead to article not found
 
 23-Oct-2009 Ian MacLennan
  # [#11578] Email cloaking does not work properly with non-ASCII characters
