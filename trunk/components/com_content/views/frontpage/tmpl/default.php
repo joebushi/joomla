2437a1cfca56968d61ce8c185ea5b91a31e39399
a.radtke <a.radtke@6f6e1ebd-4c2b-0410-823f-f34bde69bce9>
2009-12-18 10:38:05 +0000

    git-svn-id: http://joomlacode.org/svn/joomla/development/branches/front_end_fixes_5@13783 6f6e1ebd-4c2b-0410-823f-f34bde69bce9

--- trunk/components/com_content/views/frontpage/tmpl/default.php
+++ trunk/components/com_content/views/frontpage/tmpl/default.php
@@ -20,54 +20,85 @@ $pageClass = $this->params->get('pageclass_sfx');
 <div class="jarticles-featured<?php echo $pageClass;?>">
 
 <?php if ($this->params->get('show_page_title', 1)) : ?>
-<h2>
+<h1>
 	<?php echo $this->escape($this->params->get('page_title')); ?>
-</h2>
+</h1>
 <?php endif; ?>
-
+<?php $i=0; ?>
 <?php if (!empty($this->lead_items)) : ?>
-<ul class="jarticles-lead">
+<div class="leading">
 	<?php foreach ($this->lead_items as &$item) : ?>
-	<li<?php echo $item->state == 0 ? ' class="system-unpublished"' : null; ?>>
-		<?php
-			$this->item = &$item;
-			echo $this->loadTemplate('item');
-		?>
-	</li>
+	<div class="leading-item<?php echo $item->state == 0 ? ' system-unpublished' : null; ?>">
+
+			<?php
+                        $this->item = &$item;
+                        $i++;
+                        echo $this->loadTemplate('item');
+                ?>
+                   <span class="article_separator">&nbsp;</span>
+
+	</div>
 	<?php endforeach; ?>
-</ul>
+</div>
 <?php endif; ?>
 
 <?php if (!empty($this->intro_items)) : ?>
-<ul class="jarticles-intro jcols-<?php echo (int) $this->columns;?>">
-	<?php foreach ($this->intro_items as $key => &$item) : ?>
-	<li class="jcolumn-<?php echo (((int)$key - 1) % (int) $this->columns)+1;?><?php echo $item->state == 0 ? ' system-unpublished"' : null; ?>">
-		<?php
-			$this->item = &$item;
-			echo $this->loadTemplate('item');
-		?>
-	</li>
-	<?php endforeach; ?>
-</ul>
 
+
+        <?php
+                $introcount = $this->params->def('num_intro_articles', 4);
+                $realintrocounter = count($this->intro_items);
+                if ($realintrocounter < $introcount) $introcount = $realintrocounter;
+
+                if ($introcount) :
+                $colcount = $this->params->def('num_columns', 2);
+
+                if ($colcount == 0) :
+                        $colcount = 1;
+                endif;
+
+                $rowcount = (int) (($introcount / $colcount) + 0.5);
+
+                for ($y = 0; $y < $rowcount; $y++) : ?>
+                        <div class="article_row<?php echo $this->escape($this->params->get('pageclass_sfx'));  echo ' row'.$y ;?>">
+                    <?php for ($z = 0; $z < $colcount ; $z++  ) : ?>
+                                    <?php
+                                                    if (isset($this->intro_items[$i])) :?>
+                                                            <div class="article_column column<?php echo $z + 1; ?> cols<?php echo $colcount; ?>" >
+                                             <?php
+                                                                                          $this->item=$this->intro_items[$i];
+                                                                                          $i++;
+                                                      echo $this->loadTemplate('item');
+                                             ?>
+                                             </div>
+                                             <span class="article_separator">&nbsp;</span>
+                                     <?php else :  ?>
+
+                                     <?php endif;  ?>
+
+                                <?php endfor; ?>
+                                <span class="row_separator<?php echo $this->escape($this->params->get('pageclass_sfx')); ?>">&nbsp;</span>
+                        </div>
+                <?php endfor;
+        endif; ?>
 <?php endif; ?>
 
 <?php if (!empty($this->link_items)) : ?>
-	<div class="jarticles-more">
-	<?php echo $this->loadTemplate('links'); ?>
-	</div>
+        <?php echo $this->loadTemplate('links');?>
 <?php endif; ?>
 
 
 <?php if ($this->params->def('show_pagination', 2) == 1  || ($this->params->get('show_pagination') == 2 && $this->pagination->get('pages.total') > 1)) : ?>
-	<div class="jpagination">
-		<?php echo $this->pagination->getPagesLinks(); ?>
-		<?php if ($this->params->def('show_pagination_results', 1)) : ?>
-			<div class="jpag-results">
-				<?php echo $this->pagination->getPagesCounter(); ?>
-			</div>
-		<?php endif; ?>
-	</div>
+        <div class="pagination">
+
+         <p class="counter">
+                <?php echo $this->pagination->getPagesCounter(); ?>
+        </p>
+                <?php echo $this->pagination->getPagesLinks(); ?>
+        </div>
+        <?php if ($this->params->def('show_pagination_results', 1)) : ?>
+
+        <?php endif; ?>
 <?php endif; ?>
 
 </div>
