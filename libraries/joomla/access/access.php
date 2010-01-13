6499bcddd5f9fa32dc679a317f78e4ba8612c465
chdemko <chdemko@6f6e1ebd-4c2b-0410-823f-f34bde69bce9>
2010-01-13 13:03:18 +0000

    Merging with trunk
    git-svn-id: http://joomlacode.org/svn/joomla/development/branches/chdemko@14157 6f6e1ebd-4c2b-0410-823f-f34bde69bce9

--- libraries/joomla/access/access.php
+++ libraries/joomla/access/access.php
@@ -99,10 +99,12 @@ class JAccess
 
 		// If the asset identifier is numeric assume it is a primary key, else lookup by name.
 		if (is_numeric($asset)) {
-			$query->where('a.id = '.(int) $asset);
+			// Get the root even if the asset is not found
+			$query->where('(a.id = '.(int) $asset.($recursive ? ' OR a.parent_id=0':'').')');
 		}
 		else {
-			$query->where('a.name = '.$db->quote($asset));
+			// Get the root even if the asset is not found
+			$query->where('(a.name = '.$db->quote($asset).($recursive ? ' OR a.parent_id=0':'').')');
 		}
 
 		// If we want the rules cascading up to the global asset node we need a self-join.
